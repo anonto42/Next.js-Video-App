34a90cf85a3d42c97bbfcc5a073230667177b511
Sohidul Islam Ananto <anonto1080@gmail.com>
2024-07-20 22:57:08 +0600

    registetion page added

---
+++ src/app/register/Layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = ( {children} : any ) => {
+  return (
+    <div className='w-full h-svh'>
+        {
+            children
+        }
+    </div>
+  )
+}
+
+export default Layout
